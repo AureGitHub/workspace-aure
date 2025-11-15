f48e3082730c3c5974cd201dbc1b9c5a99eeea4b
AureGitHub <auredeveloper@gmail.com>
2025-11-15 11:02:18 +0100

    deploy 3

--- backend/app-alquiler/deploy.bat
+++ backend/app-alquiler/deploy.bat
@@ -1,1 +1,2 @@
+Remove-Item -Recurse -Force "$env:LOCALAPPDATA\deno"
 deployctl deploy --project=app-arriendo --env-file  --entrypoint=./src/main.ts  --import-map=deno.deploy.json
--- backend/common-lib/src/utils/mod.ts
+++ backend/common-lib/src/utils/mod.ts
@@ -1,4 +1,4 @@
 // Utils Module Exports
 export * from "./utils.ts";
 
-export { load } from "@std/dotenv";
+export { load } from "https://jsr.io/@std/dotenv/0.225.5/mod.ts";
