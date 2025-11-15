a1836b201bbcab0154b3646d7cf27b10f4760111
AureGitHub <auredeveloper@gmail.com>
2025-11-15 11:14:39 +0100

    fin deploy!!!!

--- backend/app-alquiler/deploy.bat
+++ backend/app-alquiler/deploy.bat
@@ -1,2 +1,4 @@
-Remove-Item -Recurse -Force "$env:LOCALAPPDATA\deno"
+powershell -Command "Get-Process deno | Stop-Process -Force"
+powershell -Command "Remove-Item -Recurse -Force $env:LOCALAPPDATA\deno"
+DENO_DEPLOY_TOKEN=ddp_ZalzIussivgjjHfX13GhmRZYVsYmYm4dwcsZ
 deployctl deploy --project=app-arriendo --env-file  --entrypoint=./src/main.ts  --import-map=deno.deploy.json
